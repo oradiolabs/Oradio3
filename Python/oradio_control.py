7e3b56a48a6d5da9d2bb006f87bb07bc93c472af
Kurantje <14157898+Kurantje@users.noreply.github.com>
2025-02-03 14:24:15 +0100

    Placeholder for testing autostart

---
+++ Python/oradio_control.py
@@ -0,0 +1,6 @@
+import oradio_utils
+print("Oradio is running!")
+oradio_utils.logging("info", "Info message in white")
+oradio_utils.logging("warning", "Warning message in yellow")
+oradio_utils.logging("error", "Error message in red")
+oradio_utils.logging("error", "Success message in green")
