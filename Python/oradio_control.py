ba7fc74878461bd8258bf92376b511c756a9f35e
Kurantje <14157898+Kurantje@users.noreply.github.com>
2025-02-03 21:29:51 +0100

    Typo's fixed

--- Python/oradio_control.py
+++ Python/oradio_control.py
@@ -1,6 +1,7 @@
 import oradio_utils
 print("Oradio is running!")
+oradio_utils.logging("debug", "Debug message in white")
 oradio_utils.logging("info", "Info message in white")
+oradio_utils.logging("success", "Success message in green")
 oradio_utils.logging("warning", "Warning message in yellow")
 oradio_utils.logging("error", "Error message in red")
-oradio_utils.logging("error", "Success message in green")
