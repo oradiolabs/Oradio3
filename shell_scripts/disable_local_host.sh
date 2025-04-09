c15cdc0224fd49652e43fae530682c69bba94ab3
HendrikV37 <henkstevens7@gmail.com>
2025-04-09 18:09:24 +0200

    Update Requirements.ods, Requirements_and_specifications.ods, and 6 more files...

---
+++ shell_scripts/disable_local_host.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Get the current IP address
+IP_ADDRESS=$(ifconfig wlan0 | grep 'inet ' | awk '{print $2}')
+# Get the current hostname
+AVAHI_HOSTNAME=$(avahi-resolve -a $IP_ADDRESS |awk '{print $2}' | cut -d'.' -f1)
+# disable the 'local' setting  for local host naming 
+sudo sed -i "s/^publish-addresses=.*/publish-addresses=no/g" /etc/avahi/avahi-daemon.conf
+sudo sed -i "s/^publish-domain=.*/publish-domain=no/g" /etc/avahi/avahi-daemon.conf
+# reload the service daemon
+sudo systemctl daemon-reload
+# restart the avahi-daemon service
+sudo systemctl restart avahi-daemon.service
