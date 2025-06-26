767abb3122f613d5f1c7d8c40f082b2b3c5f4898
olaf-at-oradio <164789031+olaf-at-oradio@users.noreply.github.com>
2025-06-26 16:48:03 +0200

    Update play_system_sound.py
    added OraadioAPstop (not yet uesed)

--- Python/play_system_sound.py
+++ Python/play_system_sound.py
@@ -64,6 +64,7 @@ SOUND_FILES = {
     "NoUSB":        f"{SOUND_FILES_DIR}/NoUSB_melding.wav",
     "WebInterface": f"{SOUND_FILES_DIR}/WebInterface_melding.wav",
     "OradioAP":     f"{SOUND_FILES_DIR}/OradioAP_melding.wav",
+    "OradioAPstop":     f"{SOUND_FILES_DIR}/OradioAP_gestopt_melding.wav",
     "WifiConnected": f"{SOUND_FILES_DIR}/Wifi_verbonden_melding.wav",
     "WifiNotConnected": f"{SOUND_FILES_DIR}/Niet_Wifi_verbonden_melding.wav",
     "NewPlaylistPreset": f"{SOUND_FILES_DIR}/Nieuwe_afspeellijst_preset.wav",
