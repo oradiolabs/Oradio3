ceff42b6a742c31896db7512174927b3bb4e10a2
Kurantje <14157898+Kurantje@users.noreply.github.com>
2025-11-27 16:46:01 +0100

    Simplified close method
    Address issue #376
    - Add code to send close command when icon is touched/clicked causingserver to close
    - Remove the code to track multiple connections

--- Python/static/js/oradio3.js
+++ Python/static/js/oradio3.js
@@ -6,30 +6,6 @@
  * @license:      GNU General Public License version 3; https://www.gnu.org/licenses/gpl-3.0.html
  */
 
-/*
- * Use WebSocket to allow server to track site closing using cookie
- */
-
-// Retrieve the value of a cookie by its name
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-    return null;
-}
-
-// Get the 'client_token' cookie
-const client_token = getCookie('client_token');
-
-// Open a WebSocket connection with the client token as a query parameter
-let ws = new WebSocket(`/ws?client_token=${client_token}`);
-
-// Log any WebSocket errors and close the connection
-ws.onerror = (err) => {
-    console.log("WebSocket error:", err);
-    ws.close();
-};
-
 // Close the web interface
 function closeWebInterface() {
 	// Show a message about closing the web interface
@@ -38,9 +14,21 @@ function closeWebInterface() {
 		'<div>De web interface wordt afgesloten...</div>' +
 	'</div>';
 
+	// Remove menu
+	document.querySelector('nav').remove();
+
 	// Show waiting indicator
 	show_waiting();
 
-	// Close the WebSocket
-	ws.close();
+	// Send close command
+	fetch('/close', {
+		method: 'POST',
+		headers: {'Content-Type': 'application/json'}
+	}).catch(() => {
+		// Handle server not responding
+		container.innerHTML = '<div class="closing">' + 
+			`<p class='error'>Er ging iets mis: Druk op de rode knop op de Oradio om de web interface af te sluiten</p>` +
+		'</div>';
+	});
 }
+
