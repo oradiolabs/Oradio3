ec0fb6fe54041a89f3f4586d3c948c39f107aae5
Kurantje <14157898+Kurantje@users.noreply.github.com>
2026-09-10 13:55:31 +0200

    Update constants.py

--- Main/constants.py
+++ Main/constants.py
@@ -95,6 +95,7 @@ SOUND_NO_WIFI      = "WifiNotConnected"
 SOUND_NO_INTERNET  = "NoInternet"
 SOUND_NEW_PRESET   = "NewPlaylistPreset"
 SOUND_NEW_WEBRADIO = "NewPlaylistWebradio"
+SOUND_POWER_ERROR  = "PowerSupplyNotSupported"
 
 ##### REMOTE SERVER #######################################
 RMS_SERVER_URL = _ENV["RMS_SERVER_URL"]
